1d14e23b6a7a5c1ed90dfa09723dd796190f66cb
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-03-15 09:53:34 +0900

    함수

--- 문법.js
+++ 문법.js
@@ -8,3 +8,20 @@ const a = 30;
 console.log(a); //30
 // a = 10; // 에러 const는 값을 바꿀 수 없다.
 
+//함수
+function add(n1,n2){
+  return n1+n2;
+}
+
+result = add(1,2);
+console.log(result); //3
+
+plus = add; //plus도 add()의 역하을 할 수 있음
+result = plus(3,4);
+console.log(result);
+
+sub = function(n1,n2){
+  return n1-n2;
+}
+result = sub(3,2);
+console.log(result); //1
