2eb42756c94556a1521d86ea894bf6bb631345b2
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-03-15 10:04:09 +0900

    한 줄로 함수 쓰기

--- 문법.js
+++ 문법.js
@@ -20,9 +20,7 @@ plus = add; //plus도 add()의 역하을 할 수 있음
 result = plus(3,4);
 console.log(result);
 
-sub = (n1,n2) => {
-  return n1-n2;
-}
+sub = (n1,n2) =>  n1-n2;
 result = sub(3,2);
 console.log(result); //1
 
