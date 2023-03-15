c4efbe8425e7da24bcd7f4d696805b60ba9c8625
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-03-15 10:03:40 +0900

    화살표함수

--- 문법.js
+++ 문법.js
@@ -20,8 +20,9 @@ plus = add; //plus도 add()의 역하을 할 수 있음
 result = plus(3,4);
 console.log(result);
 
-sub = function(n1,n2){
+sub = (n1,n2) => {
   return n1-n2;
 }
 result = sub(3,2);
 console.log(result); //1
+
