11ce5a4af80ea471097f305bddd483a9979394e2
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-03-22 09:47:37 +0900

    null&undefined

---
+++ null&undefined.js
@@ -0,0 +1,18 @@
+//null : 존재하지 않거나 유효하지 않은 object 의도적으로 비어있음을 표현할 때 사용
+//undefined : 값이 명시되지 않은 변수
+
+let foo;
+console.log(foo); //undefined
+
+let obj = {};
+console.log(obj.prop); //undefined
+
+//위에 두 변수는 선언을 하였으나, 이번에는 선업 안 했다
+let bar = null;
+console.log(bar); //에러
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+console.log(null == undefined); //true
+console.log(null === undefined); //false
