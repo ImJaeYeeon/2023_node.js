ff6fb730927222825bd56e4a970763b3234eaeec
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-04-05 09:20:27 +0900

    사진관리 초기설정 및 fs.reddSync

---
+++ fs.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+console.log(_dirname);
+const files = fs.readdirSync(_dirname);
+console.log(files);
--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-const http = require('http');
-// const fs = require('fs');
-const url = require('url');
+const fs = require('fs');
+const path = require('path');
 
-const app = http.createServer(function(req, res) {
-    let _url = req.url; //문자열 형태
-    //문자열 형태의 _url를 object 형태로 변환
-    let queryData = url.parse(_url, true).query;
-    res.end(queryData.id);
-});
-
-app.listen(2222);
+function getAllFiles(dirpath,arrOffFiles){
+  const files = fs.readdirSync(dirPath);
+}
