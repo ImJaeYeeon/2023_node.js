41e887d14be508ef476dd3541650f1523bfa5719
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-04-12 09:55:55 +0900

    path.join()

--- fs.js
+++ fs.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 
 //__dirname: 현재 디렉토리의 절대경로(Node의 전역 변수)
-console.log(_dirname);
+result = path.join(__dirname,files[i]);
+console.log(result);
 
 //해당 경로에 있는 파일 목록의 배열을 반환
-const files = fs.readdirSync(__dirname);
+const files = fs.readdirSync(result);
 
 //특정 파일의 전체(절대) 경로
 result = __dirname + '\\' + files[1];
