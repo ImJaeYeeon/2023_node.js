b1836b3ba312b7c23324e1df08172fccb05a3992
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-04-19 09:49:53 +0900

    사진관리

--- 사진관리/main.js
+++ 사진관리/main.js
@@ -21,5 +21,53 @@ function getAllFiles(dirPath, arrOfFiles) {
   return arrOfFiles;
 }
 
-const files = getAllFiles(__dirname + "\\base", []);
+function getDuplicated(baseDir) {
+  
+  // 해당 디렉토리가 존재하는지 유효성 검사
+  if(!fs.existsSync(baseDir)) {
+    console.error('폴더가 존재하지 않음');
+    return;
+  }
+
+  const duplicatedDir = path.join(baseDir, 'duplicated');
+
+  if(!fs.existsSync(duplicatedDir))
+    fs.mkdirSync(duplicatedDir);
+  
+  // base폴더에 있는 모든 파일들 list
+  const arrayOfFiles = getAllFiles(baseDir, []);
+  
+  // 중복된 파일들(절대경로) list
+  const duplicatedFiles = [];
+  
+  // file : 파일의 절대경로(string)
+  arrayOfFiles.forEach(function (file, idx){
+    // 절대경로에서 파일이름만 추출
+    const fileName = path.basename(file);
+
+    // 같은 파일명을 가진 경로의 idx. 없으면 -1을 반환
+    const duplicatedIdx = arrayOfFiles.findIndex(function(otherFile, otherIdx){
+      if(otherIdx > idx && otherFile.includes(fileName))
+        return true;
+      else
+        return false;
+    });
+
+    // 중복된 파일이 존재한다면
+    if(duplicatedIdx > -1)
+    {
+      const duplicatedFile = arrayOfFiles[duplicatedIdx];
+
+      duplicatedFiles.push(file);             
+      duplicatedFiles.push(duplicatedFile);   // 중복된 파일의 절대경로
+    }
+  });
+  return duplicatedFiles;
+}
+
+
+// const files = getAllFiles(path.join(__dirname, 'base'), []);
+// console.log(files.join('\n'));
+
+const files = getDuplicated(path.join(__dirname, 'base'));
 console.log(files.join('\n'));
